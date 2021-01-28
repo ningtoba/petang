7edd66a6aa737a77a9536c80ae0135fbc019c6e0
Muhammad Luqman Hakim <jawbajaw40@gmail.com>
2021-01-28 21:37:40 +0800

    WIP: final styling

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
   <div>
     <main class="p-5 h-screen">
       <div class="search-box w-full mb-3">
-        <input type="text" class="search-bar block w-full p-3 appearance-none border-none bg-none bg-white opacity-50 outline-none focus:outline-none focus:opacity-70 focus:rounded-tl-xl focus:rounded-br-xl focus:rounded-bl-none focus:rounded-tr-none shadow-xl rounded-bl-xl rounded-tr-xl transition-all duration-300" placeholder="Search...">
+        <input type="text" class="search-bar block w-full p-3 appearance-none border-none bg-none bg-white opacity-50 outline-none focus:outline-none focus:opacity-70 focus:rounded-tl-xl focus:rounded-br-xl focus:rounded-bl-none focus:rounded-tr-none focus:shadow-lg rounded-bl-xl rounded-tr-xl transition-all duration-300" placeholder="Search...">
       </div>
 
       <div class="weather-wrap">
         <div class="locaton-box">
-          <div class="location text-white text-2xl font-bold text-center ">Cyberjaya, Malaysia</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="location text-white text-2xl font-bold text-center text-shadow-md">Cyberjaya, Malaysia</div>
+          <div class="date text-white text-xl font-light text-center text-shadow-md">Monday 20 January 2020</div>
         </div>
-        <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+        <div class="weather-box text-center">
+          <div class="temp inline-block px-5 py-6 m-4 text-white text-7xl font-extrabold text-shadow-md bg-gray-50 bg-opacity-25 rounded-lg shadow-md">9°c</div>
+          <div class="weather text-white text-2xl font-bold text-shadow-md">Rain</div>
         </div>
       </div>
     </main>
@@ -45,9 +45,5 @@ export default {
   #app {
     background-color: rgb(144, 207, 209);
     transition: 0.4s;
-  }
-
-  .location-box .location {
-    text-shadow: .2rem .6rem rgba(0, 0, 0, 0.25);
   }
 </style>
